d50b2b918f92497e44882c87fd5f46995790ca82
Jon Struve <ocdammo3@gmail.com>
2019-09-20 16:36:25 -0500

    Finished categories and products both with tests

--- data-modeling/models-singular/categories-schema.js
+++ data-modeling/models-singular/categories-schema.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 
 // What fields and constraints do we want?
 const categories = mongoose.Schema({
-
+  name: { type: String, required: true },
+  description: { type: String },
 });
 
 // Do we need to run any lifecycle hooks/middleware?
